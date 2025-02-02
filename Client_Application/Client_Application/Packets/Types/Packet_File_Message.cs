2c2eb7a171dd7f4e5d48183e88ac17489742c2b8
WEI-HAN CHEN <116339924+henrycwh@users.noreply.github.com>
2025-02-03 00:34:39 +0800

    Add files via upload

---
+++ Client_Application/Client_Application/Packets/Types/Packet_File_Message.cs
@@ -0,0 +1,46 @@
+﻿using Client_Application.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client_Application.Packets.Types
+{
+    internal class Packet_File_Message : Packet_Base
+    {
+        private byte[] File;
+        public Packet_File_Message(byte[] data) : base(data) 
+        { 
+            packet_type = Packet_Type.File_Message;
+            File = data;
+        }
+
+        public override void Handle()
+        {
+            Window current_window = Global.Get_Current_Window();
+            if (current_window == null) return;
+            if (!(current_window is Chat_Room_Page)) return;
+            Global.Run_Task_On_UI(new Task(() => ((Chat_Room_Page)current_window).Add_File(File, Controls.Message_Block.Text_Direction.LEFT)));
+        }
+        public byte[] Get_File() => File;
+        public override byte[] Encode_Packet()
+        {
+            byte[] encoded = File;
+            int length = encoded.Length;
+            List<byte> result = new List<byte>();
+            result.Add((byte)length);
+            result.AddRange(encoded);
+            return result.ToArray();
+        }
+        public override void Decode_Packet()
+        {
+            int length = File[0];
+            byte[] bytes = new byte[length];
+            for (int i = 0; i < length; i++)
+                bytes[i] = File[i + 1];
+            File = bytes;
+        }
+    }
+}
---
+++ Client_Application/Client_Application/Pages/Chat_Room_Page.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Client_Application.Controls;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using static Client_Application.Controls.Message_Block;
+using System.IO;
+
+namespace Client_Application.Pages
+{
+    public partial class Chat_Room_Page : Window
+    {
+        public Chat_Room_Page()
+        {
+            InitializeComponent();
+            Loaded += delegate
+            {
+                Add_Broadcast_Message("歡迎您的加入。");
+            };
+            Closing += delegate { Global.Disconnect_From_Server(); };
+        }
+        public void Add_Broadcast_Message(string Text) => Add_Text(Text, Text_Direction.CENTER);
+
+        public void Add_Text(string Text, Text_Direction Direction)
+        {
+            Message_Block block = new Message_Block();
+            block.Set_Text(Text);
+            block.Set_Alignment(Direction);
+            Message_Container.Children.Add(block);
+        }
+        public void Add_File(byte[] File, Text_Direction Direction)
+        {
+            Message_Block block = new Message_Block();
+            block.Set_Text(File);
+            block.Set_Alignment(Direction);
+            Message_Container.Children.Add(block);
+        }
+
+        private void On_Key_Down(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                Global.Get_Server().Send_Message(Input_Message.Text);
+                Input_Message.Text = "";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog openfiledialog = new System.Windows.Forms.OpenFileDialog();
+            if(openfiledialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string Selected_File = openfiledialog.FileName;
+                try
+                {
+                    // 使用 FileStream 來讀取二進制檔案
+                    using (FileStream fileStream = new FileStream(Selected_File, FileMode.Open, FileAccess.Read))
+                    {
+                        byte[] buffer = new byte[fileStream.Length];
+                        int bytesRead = fileStream.Read(buffer, 0, buffer.Length);
+                        Global.Get_Server().Send_File(buffer);
+                        // 在這裡可以處理讀取到的二進制資料，例如播放 mp3 檔案
+                        MessageBox.Show($"成功讀取 {bytesRead} 位元組的 MP3 檔案。", "成功", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"讀取 MP3 檔案失敗: {ex.Message}", "錯誤", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
